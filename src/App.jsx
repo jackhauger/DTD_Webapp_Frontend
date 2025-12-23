ab80a5f5c31b0e2a47019486971eef37b4665d06
jackhauger <jackhauger5@gmail.com>
2025-12-23 14:40:31 -0500

    deployment test 2

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Policy Database</h1>
+      <p>Frontend deployed successfully.</p>
+    </div>
+  );
+}
